317e3a289e702a065b34561b11d27ad2cfbdd059
ThimnaShushu <thimnashushu@gmail.com>
2025-04-07 22:46:09 +0200

    The To Do List is Able to add tasks to the taks list.

--- toDoList.java
+++ toDoList.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class toDoList {
+
+    static ArrayList<String> taskList = new ArrayList<String>();
+    String task;
+
     public static void main(String[] args) {
         System.out.println("Welcome To Your To-Do List!");
+        Scanner scanner = new Scanner(System.in);
+        int menuNum = 0;
+        String newTask;
+
+        while (menuNum != 4) {
+            System.out.println(
+                    "1. Display To-Do List \n2. Add New Task To Do \n3. Remove Task From \n4. Exit To-Do List ");
+            menuNum = scanner.nextInt();
+
+            switch (menuNum) {
+                case 1:
+                    break;
+                case 2:
+                    System.out.print("Add Your New Task: ");
+                    newTask = scanner.next();
+                    addTask(newTask);
+                    break;
+                case 3:
+                    break;
+
+            }
+
+        }
+
     }
+
+    public static void addTask(String task) {
+        taskList.add(task);
+        System.out.println("New Task Has Been Added Successfully!");
+
+    }
+
 }

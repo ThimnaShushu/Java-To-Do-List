3a972859994cc0a849eeab59b0441bf505f38785
ThimnaShushu <thimnashushu@gmail.com>
2025-04-08 22:09:46 +0200

    The To Do List is fully functional using the terminal

--- toDoList.java
+++ toDoList.java
@@ -11,14 +11,25 @@ class toDoList {
         Scanner scanner = new Scanner(System.in);
         int menuNum = 0;
         String newTask;
+        int remTask;
 
         while (menuNum != 4) {
             System.out.println(
-                    "1. Display To-Do List \n2. Add New Task To Do \n3. Remove Task From \n4. Exit To-Do List ");
+                    "1. Display To-Do List \n2. Add New Task To Do \n3. Remove Task From To-Do List\n4. Exit To-Do List ");
             menuNum = scanner.nextInt();
 
             switch (menuNum) {
                 case 1:
+                    if (taskList.size() != 0) {
+                        System.out.println("=================================");
+                        System.out.println("To-Do List:");
+                        for (int i = 0; i < taskList.size(); i++) {
+                            System.out.println((i + 1) + ". " + taskList.get(i));
+                        }
+                        System.out.println("=================================");
+                    } else {
+                        System.out.println("Your To-Do List is Empty!");
+                    }
                     break;
                 case 2:
                     System.out.print("Add Your New Task: ");
@@ -26,11 +37,16 @@ class toDoList {
                     addTask(newTask);
                     break;
                 case 3:
+                    System.out.print("Which Task Do You Want To Remove? ");
+                    remTask = scanner.nextInt();
+                    taskList.remove(remTask - 1);
+                    System.out.println("Task number " + remTask + " has been removed successfully!");
                     break;
 
             }
 
         }
+        System.out.println("Goodbye!");
 
     }
 
